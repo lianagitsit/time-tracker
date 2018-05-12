b8f07ed842e5ec4f2bd9e5d2ea0286b541330edf
Liana Mancini <liana.mancini@gmail.com>
2018-05-12 19:25:29 -0400

    establishes dependencies, file structure, basic server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var db = require("./models");
+
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./routes/api-routes.js");
+
+app.use(routes);
+
+db.sequelize.sync().then( function() {
+  app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+})
