f0717a29eb022bb6181e38f0f71b83a05225968e
Liana Mancini <liana.mancini@gmail.com>
2018-05-13 22:02:38 -0400

    displays database info to the page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,15 @@
 var path = require("path");
+var db = require("../models");
 
 module.exports = function(app){
 
     app.get("/", function(req, res) {
-        var myObj = { 
-            items: ["thingOne", "thingTwo"] 
-        };
-        
-        res.render("index", myObj);
+        db.User.findAll().then( function(dbUser){
+            var userObj = {
+                users: dbUser
+            }
+            res.render("index", userObj);
+        })
     });
+
 }
