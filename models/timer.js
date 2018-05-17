eb41b749e0299283d51530c6806fb5c64c6285e9
Thompson <thompa@corp.uhsinc.biz>
2018-05-17 19:59:34 -0400

    got the timer to start...then it threw an error

--- models/timer.js
+++ models/timer.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     $("#start").on("click", function(){
-        console.log("booty");
+        console.log("button works");
+        clock.start();
+        
     })
     
 };
@@ -8,16 +10,35 @@ window.onload = function () {
 var intervalId;
 var clockRunning = false;
 
-var clock = function () {
-    time: 0
-    start= function () { 
+var clock =  {
+    time: 0,
+    start: function () { 
         if (!clockRunning) {
             intervalId = setInterval(clock.count, 1000);
             clockRunning = true;
         }
+    },
+    count: function () {
+        clock.time++;
+        var converted = clock.timeConverter(clock.time);
+        console.log(converted);
+        $("#timer").text(converted);
+
+    },
+    timeConverter:  function(t){
+        var minutes = Math.floor(t/60);
+        var seconds = Math.floor= t - (minutes * 60);
+        if (seconds < 10){
+            seconds = "0" + seconds;
+        };
+        if (minutes === 0){
+            minutes = "00";
+        }
+        else if (minutes < 10){
+            minutes = "0" + minutes
+        }
+        return minutes + ":" + seconds;
     }
 }
 
-count= function () {
-    clock.time++;
-}
+
