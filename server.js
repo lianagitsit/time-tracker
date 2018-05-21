5c6df8cf0501d45d5007a3b93faaa6b4cb5609df
Liana Mancini <liana.mancini@gmail.com>
2018-05-21 17:00:53 -0400

    testing passport-google-oauth

--- server.js
+++ server.js
@@ -1,25 +1,62 @@
+require("dotenv").config();
+
+// Server
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+
+// Auth
+var passport = require("passport");
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+// Database
 var db = require("./models");
 
-var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
-
+// CONFIG
 var app = express();
 
-app.use(express.static("public"));
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// AUTH
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:8080/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+     db.User.findOrCreate({ googleId: profile.id }, function (err, user) {
+       return done(err, user);
+     });
+}
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ROUTES
+app.use(express.static("public"));
 
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
+// START SERVER
 db.sequelize.sync().then( function() {
   app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
