d524d401396618ece66df94b035057295f8e8164
Liana Mancini <liana.mancini@gmail.com>
2018-05-22 10:24:46 -0400

    attempt accessing calendar from server, incl destructuring bug

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,26 +1,66 @@
 var db = require("../models");
 var passport = require("passport");
+const { google } = require('googleapis');
 
 module.exports = function (app) {
 
     app.get('/auth/google',
         passport.authenticate('google', { scope: ['profile'] }
+        ));
+
+    // Maybe idk what this does or if I need it
+    app.get('/auth/google/calendar', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/calendar.readonly'] }
     ));
 
-
-    app.get('/auth/google/callback', 
+    app.get('/auth/google/callback',
         passport.authenticate('google', { failureRedirect: '/login' }),
-        function(req, res) {
-          res.redirect('/secret');
-    });
+        function (req, res) {
+            res.redirect('/secret');
+        });
 
+    // app.get('/secret',
+    //     require('connect-ensure-login').ensureLoggedIn(),
+    //     function(req, res){
+    //         var userObj = req.user;
+    //         res.render('secret', { user: userObj });
+    // });
+
+    // Test stuff, who knows, use the above original
     app.get('/secret',
         require('connect-ensure-login').ensureLoggedIn(),
-        function(req, res){
+        function (req, res) {
+            // console.log("REQUEST");
+            // console.log(req);
+            // console.log("RES");
+            // console.log(res);
+            listEvents(passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/calendar.readonly'] }));
             var userObj = req.user;
             res.render('secret', { user: userObj });
-    });
-  
+        });
+
+    // Copied from the node quickstart, who knows
+    function listEvents(auth) {
+        const calendar = google.calendar({ version: 'v3', auth });
+        calendar.events.list({
+            calendarId: 'primary',
+            timeMin: (new Date()).toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime',
+        }, (err, { data }) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const events = data.items;
+            if (events.length) {
+                console.log('Upcoming 10 events:');
+                events.map((event, i) => {
+                    const start = event.start.dateTime || event.start.date;
+                    console.log(`${start} - ${event.summary}`);
+                });
+            } else {
+                console.log('No upcoming events found.');
+            }
+        });
+    }
 
 
     ////////////////
@@ -43,12 +83,12 @@ module.exports = function (app) {
         });
     });
 
-    app.post("/api/time", function(req, res) {
+    app.post("/api/time", function (req, res) {
         var activityTime = req.body.time;
         db.Activity.create({
             name: "general",
             time: activityTime
-        }).then( dbActivity => {
+        }).then(dbActivity => {
             res.send(dbActivity);
         });
 
